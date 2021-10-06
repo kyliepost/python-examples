3ffaa3f5e7a8cc73cde2599f009c21a7d910e352
kyliepost <kylie.postlethwaite00@gmail.com>
2021-10-06 13:55:35 -0500

    inital commit

---
+++ python.py
@@ -0,0 +1,21 @@
+# PRINTING
+# print('Welcome to python')
+# print('Welcome to python!!!!!')
+
+# NAMING VARIABLES AND PRINTING ASSIGNMENTS
+#item_name = 'strawberry'
+#price = 2.39
+#inventory = 15
+#is_in_inventory = True
+#print(item_name, price, inventory)
+
+# BASIC ARITHMETIC OPERATIONS
+a=6
+b=2
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
