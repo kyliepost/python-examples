735a99c60f6072f79019e52cf036f2800af575b6
kyliepost <kylie.postlethwaite00@gmail.com>
2021-10-07 11:10:39 -0500

    string basics completed

--- python.py
+++ python.py
@@ -2,12 +2,16 @@
 # print('Welcome to python')
 # print('Welcome to python!!!!!')
 
+
+
 # NAMING VARIABLES AND PRINTING ASSIGNMENTS
 #item_name = 'strawberry'
 #price = 2.39
 #inventory = 15
 #is_in_inventory = True
 #print(item_name, price, inventory)
+
+
 
 # BASIC ARITHMETIC OPERATIONS
 #a=6
@@ -20,23 +24,25 @@
 # print('Modulus : ', a % b)
 # print('Exponent : ', a ** b)
 
+
+
 # STRING BASICS/SLICING
 msg='welcome to python'
     #0123456789 
 # print(msg)
-# WANT IT TO PRINT TWICE?
+    # WANT IT TO PRINT TWICE?
 # print(msg+msg)
 # print(msg*2)
 # print(msg,msg)
-#WANT TO CHANGE CASING?
+    #WANT TO CHANGE CASING?
 # print(msg.upper())
 # print(msg.lower())
 # print(msg.capitalize())
 # print(msg.title())
-# WANT TO KNOW INFO ABOUT STRING?
+    # WANT TO KNOW INFO ABOUT STRING?
 # print(len(msg))
 # print(msg.count('python'))
-# SLICING STRINGS
+    # SLICING STRINGS
     # THE WAY TO ACCESS NUMBERS IS BY USING []
 # print(msg[4])
     # CAN ALSO USE NEGATIVE INDEXES(-)
@@ -55,8 +61,33 @@ msg='welcome to python'
 # # print(msg)
 # print((msg4 + ' ' + msg[:7] + ' ' + msg2 + ' ' + msg[8:10] + ' ' + msg3).title())
     # OR
-msg='welcome to Python 101: Strings'
-msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
-print(msg1.title())
+# msg='welcome to Python 101: Strings'
+# msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
+# print(msg1.title())
     # WANT TO PRINT BACKWARDS?
-print(msg1[::-1].title())
+# print(msg1[::-1].title())
+    # WANT MULTIPLE LINES? - USE TRIPLE QUOTES AT BEGINNING AND END, USE RETURNS TO BREAK LINES
+# msg="""Hello Python!
+# I like Python!"""
+# print(msg)
+    # NEED TO FIND SOMETHING?
+# print(msg.find('python'))
+    # NEED TO REPLACE SOMETHING?
+# print(msg.replace('python','Javascript'))
+    # TO SAVE, YOU NEED TO STORE IN A VARIABLE
+# msg1=msg.replace('python','Javascript')
+# print(msg1)
+    # MEMBERSHIP - CHECKS OF SPECIFIC ITEM IS IN MSG (COMES BACK T OR F) - NOT EXIST? USE NOT IN 
+# print('python' in msg)
+# print('python' not in msg)
+    # STRING FORMATTING
+# name='TERRY'
+# color = 'RED'
+# msg = '[' + name + '] loves the color ' + color + '!'
+# msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+# print(msg)
+# print(msg1)
+
+
+
+# USER INPUT
