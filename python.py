153c36d294744da387ce7042450d676f15fd6d59
kyliepost <kylie.postlethwaite00@gmail.com>
2021-10-07 09:18:48 -0500

    practice

--- python.py
+++ python.py
@@ -10,12 +10,20 @@
 #print(item_name, price, inventory)
 
 # BASIC ARITHMETIC OPERATIONS
-a=6
-b=2
-print('Addition : ', a + b)
-print('Subtraction : ', a - b)
-print('Multiplication : ', a * b)
-print('Division (float) : ', a / b)
-print('Division (floor) : ', a // b)
-print('Modulus : ', a % b)
-print('Exponent : ', a ** b)
+#a=6
+# b=2
+# print('Addition : ', a + b)
+# print('Subtraction : ', a - b)
+# print('Multiplication : ', a * b)
+# print('Division (float) : ', a / b)
+# print('Division (floor) : ', a // b)
+# print('Modulus : ', a % b)
+# print('Exponent : ', a ** b)
+
+# STRING BASICS/SLICING
+msg='welcome to python'
+print(msg)
+    # WANT IT TO PRINT TWICE?
+    print(msg+msg)
+    print(msg*2)
+    print(msg,msg)
